207259ea08ce75973bef0a020a137fab1556b55c
Pravin Suryawanshi <pravinsuryawanshi1999@gmail.com>
2025-04-12 16:03:10 +0530

    home_creadit_defaukt_risk_setup

---
+++ ingress/metrix_k8_install.sh
@@ -0,0 +1,7 @@
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+
+
+# edit the deployment for insecure connection
+
+#- --kubelet-insecure-tls
