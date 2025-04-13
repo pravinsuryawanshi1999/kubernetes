3426d22e56235ed9a78c26d54c01e7aacc92fe6e
Pravin Suryawanshi <pravinsuryawanshi1999@gmail.com>
2025-04-13 11:00:28 +0530

    create a shell script for docker login

--- Scripts/docker-login.sh
+++ Scripts/docker-login.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "$1" | /usr/local/bin/docker login "$2" --username "$3" --password-stdin
+echo "$1" | /usr/local/bin/docker  --config /tmp/.docker  login "$2" --username "$3" --password-stdin
 
